8835d30225f66558d1e9249e5af6b53d0edeb9ba
Sergey Kanzhelev <SergKanz@microsoft.com>
2017-11-10 23:05:24 -0800

    mend

--- src/Adapters/NLogTarget.Shared/AssemblyInfo.cs
+++ src/Adapters/NLogTarget.Shared/AssemblyInfo.cs
@@ -16,7 +16,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.ApplicationInsights.NLogTarget")]
 [assembly: AssemblyDescription("Application Insights NLog Target is a custom target allowing you to send NLog log messages to Application Insights. Application Insights will collect your logs from multiple sources and provide rich powerful search capabilities. This functionality depends on Microsoft Monitoring Agent being installed onto your machine, http://go.microsoft.com/fwlink/?LinkID=328052.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
