eda58462e13c2bf2ed149c5d49924728e6c135d1
Timothy Mothra Lee <tilee@microsoft.com>
2018-01-02 14:54:45 -0800

    binSkim script to prep build server for SDL scan

---
+++ SDL/BinSkimSetup.ps1
@@ -0,0 +1,38 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$buildDirectory,
+    
+    [Parameter(Mandatory=$True)]
+    [string]$binSkimDirectory
+)
+
+#$buildDirectory = "C:\Repos\SDK\bin\Release\Src\"
+#$binSkimDirectory = "C:\Repos\SDK\binSkim\"
+
+Write-Host "`nPARAMETERS:";
+Write-Host "`tbuildDirectory:" $buildDirectory;
+Write-Host "`tbinSkimDirectory:" $binSkimDirectory;
+
+# don't need to clean folder on build server, but is needed for local dev
+if (Test-Path $binSkimDirectory) { Remove-Item $binSkimDirectory -Recurse; }
+
+# copy all
+Copy-Item -Path $buildDirectory -Filter "*.dll" -Destination $binSkimDirectory -Recurse;
+
+# delete test directories
+Get-ChildItem -Path $binSkimDirectory -Recurse -Directory | 
+ Where-Object {$_ -match "Test"} |
+ Remove-Item -Recurse;
+
+# summary for log output (file list and count)
+Write-Host "`nCOPIED FILES:";
+
+$count = 0;
+Get-ChildItem -Path $binSkimDirectory -Recurse -File | 
+    ForEach-Object { 
+        Write-Host "`t"$_.FullName; 
+        $count++;
+    } 
+
+Write-Host "`nTOTAL FILES:" $count;
